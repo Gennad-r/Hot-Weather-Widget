e80c3c5f5f22a23ecb28a8f3782eaa189441cfea
Gennad-r <gennad.r@gmail.com>
2019-12-19 18:11:10 +0200

    unsubscribing added

--- src/app/hotels/list/list.component.ts
+++ src/app/hotels/list/list.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Activity } from 'src/app/models/activity';
 import { MainService } from 'src/app/main.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
+  subscribes: Subscription[];
   activities: Activity[];
   constructor(
     public mainServise: MainService
@@ -15,9 +17,14 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.mainServise.getActivities().subscribe(items => {
+    const d = this.mainServise.getActivities().subscribe(items => {
       this.activities = [...items];
     });
+    this.subscribes.push(d);
+  }
+
+  ngOnDestroy() {
+    this.subscribes.forEach(s => s.unsubscribe);
   }
 
 }
