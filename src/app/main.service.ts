f15f21da9527c377f9f21ac9defcef008e12a0cd
Gennad-r <gennad.r@gmail.com>
2019-12-19 21:14:55 +0200

    fix import

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Activity } from './models/activity';
 
 @Injectable({
